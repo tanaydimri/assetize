4fba07f9b7fbce333604c90d50ca50c373802163
Tanay Dimri <tanay.dim@gmail.com>
2020-07-30 19:29:52 +0530

    Revamped the UI to add the functionality of supporting Stages and added tab Widgets

--- src/assetize.py
+++ src/assetize.py
@@ -1,5 +1,6 @@
 import os
 
+from PySide2 import QtGui
 from maya import cmds
 import maya.mel as mel
 
@@ -20,26 +21,117 @@ class Assetize(object):
             print "Invalid File: {0}".format(filepath)
             return "Invalid File: '{0}'".format(filepath)
 
-        asset_name_raw = "ASSET_{0}_{1}".format(
+        asset_name = "ASSET_{0}_{1}".format(
             os.path.splitext(os.path.basename(filepath))[0], self.placer_prefix
         )
-
-        counter = 0
-        asset_name = asset_name_raw
-
-        while True:
-            if asset_name in self.assets.keys():
-                counter += 1
-                counter_string = str(counter).zfill(2)
-                asset_name = "{0}_{1}".format(asset_name_raw, counter_string)
-            else:
-                break
 
         # TODO: Remove all special characters from the asset name
         return asset_name
 
-    def register_asset(self, asset_name, asset_ref_path):
-        self.assets[asset_name] = asset_ref_path
+    @staticmethod
+    def construct_flavour(
+        flavour_path,
+        flavour_name="main",
+        fg_color=(255, 255, 255, 255),
+        bg_color=(0, 0, 0, 0),
+    ):
+        """
+
+        :param string flavour_path: Path of the flavour file on the disk.
+        :param string flavour_name: Name of the flavour that needs to be registered. Defaults to
+                                    "main".
+        :param tuple fg_color: Text Color to be assigned to the flavour. Defaults to Red.
+        :param tuple bg_color: Background Color to be assigned to the flavour. Defaults to Red.
+
+        :return dict: Packaged information in a data structure to be registered on the asset.
+        """
+        return {
+            flavour_name: {
+                "disk_path": flavour_path,
+                "fg_color": fg_color,
+                "bg_color": bg_color,
+            }
+        }
+
+    def register_main_asset(
+        self,
+        asset_name,
+        flavour_path,
+        flavour_name="main",
+        fg_color=(255, 255, 255, 255),
+        bg_color=(0, 0, 0, 0),
+    ):
+        """
+        Registers the main asset in the assets dict.
+
+        {
+            "<Asset Name>": {
+                "flavours": {
+                    "main": {
+                        "<disk_path": "<path on disk>",
+                        "fg_color": QtGui.QColor(255, 0, 0, 255),
+                        "bg_color": QtGui.QColor(0, 0, 0, 0),
+                    },
+                    "<flavour_01>": {
+                        "<disk_path": "<path on disk>",
+                        "fg_color": QtGui.QColor(255, 0, 0, 255),
+                        "bg_color": QtGui.QColor(0, 0, 0, 0),
+                    }
+                }
+            }
+        }
+
+        :param string asset_name: Name of the asset that needs to be registered.
+        :param string flavour_path: Path of the flavour file on the disk.
+        :param string flavour_name: Name of the flavour that needs to be registered. Defaults to
+                                    "main".
+        :param tuple fg_color: Text Color to be assigned to the flavour. Defaults to Red.
+        :param tuple bg_color: Background Color to be assigned to the flavour. Defaults to Red.
+        """
+        self.assets[asset_name] = {
+            "flavours": self.construct_flavour(
+                flavour_path=flavour_path,
+                flavour_name=flavour_name,
+                fg_color=fg_color,
+                bg_color=bg_color,
+            )
+        }
+
+        from pprint import pprint
+
+        pprint(self.assets)
+
+    def add_flavour(self, asset_name, flavour_name, flavour_path):
+        """
+        Add flavour to an existing asset.
+        """
+        self.assets[asset_name]["flavours"].update(
+            self.construct_flavour(
+                flavour_name=flavour_name,
+                flavour_path=flavour_path,
+                fg_color=(255, 0, 0, 255),
+            )
+        )
+
+    def update_asset(
+        self, asset_name, new_asset_name=None, asset_ref_path=None, fg_color=None
+    ):
+
+        if new_asset_name:
+            self.assets[new_asset_name] = self.assets.get(asset_name)
+            self.assets.pop(asset_name)
+            cmds.rename(asset_name, new_asset_name)
+
+        if asset_ref_path is not None:
+            self.assets[asset_name]["asset_path"] = asset_ref_path
+
+        if fg_color:
+            self.assets[asset_name] = {
+                "asset_path": asset_ref_path,
+                "fg_color": fg_color,
+            }
+
+        print self.assets
 
     def deregister_asset(self, asset_name):
         self.assets.pop(asset_name)
@@ -60,3 +152,9 @@ class Assetize(object):
     @staticmethod
     def delete_asset_node(asset_name):
         cmds.delete(asset_name)
+
+    def save_assets(self):
+        json_filepath = QtCore.QFileDialog
+
+        with open(json_filepath, "w") as json_f:
+            json.dump(dictionary_data, json_f, indent=4)
--- src/assetize_ui.py
+++ src/assetize_ui.py
@@ -1,6 +1,5 @@
 """
 
-
 """
 # importing builtin modules
 import os
@@ -8,6 +7,7 @@ import os
 # importing third party modules
 from maya import cmds
 import maya.OpenMayaUI as omui
+from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
 from shiboken2 import wrapInstance
 from PySide2 import QtGui, QtCore, QtUiTools, QtWidgets
 
@@ -28,6 +28,10 @@ def maya_main_window():
 
 
 class AssetStandardItem(QtGui.QStandardItem):
+    """
+    Asset Item for the tree view to be populated. This would be created per asset that is populated.
+    """
+
     def __init__(
         self,
         content,
@@ -35,7 +39,7 @@ class AssetStandardItem(QtGui.QStandardItem):
         font_size=8,
         set_bold=False,
         set_editable=False,
-        fg_color=QtGui.QColor(0, 0, 0, 0),
+        fg_color=QtGui.QColor(255, 255, 255, 255),
         bg_color=QtGui.QColor(0, 0, 0, 0),
     ):
         super(AssetStandardItem, self).__init__()
@@ -50,7 +54,7 @@ class AssetStandardItem(QtGui.QStandardItem):
         self.setText(content)
 
 
-class AssetizeUI(QtWidgets.QDialog):
+class AssetizeUI(MayaQWidgetDockableMixin, QtWidgets.QDialog):
     def __init__(
         self,
         assetize_backend,
@@ -67,6 +71,8 @@ class AssetizeUI(QtWidgets.QDialog):
         self.tree_model = QtGui.QStandardItemModel()
         self.assets_tree_root_node = None
         self.assetize_backend = assetize_backend
+        self.current_selected_indexes = []
+        self.current_asset_name = None
 
         super(AssetizeUI, self).__init__(parent)
 
@@ -89,6 +95,31 @@ class AssetizeUI(QtWidgets.QDialog):
         self.update_tree_view()
         self.setup_connections()
 
+    @staticmethod
+    def get_file_path_dialog(
+        start_dir=os.path.expanduser("~"),
+        file_filter="All Files *.*;;Maya Files (*.ma *.mb);;Alembic Files (*.abc)",
+        caption="OK",
+    ):
+        """
+        Opens up a new FIle Dialog to get the Asset FIle Path from the user.
+
+        :return string: Path to the asset on disk as selected by the user.
+        """
+        file_paths = cmds.fileDialog2(
+            startingDirectory=start_dir,
+            fileFilter=file_filter,
+            fileMode=1,
+            okCaption=caption,
+        )
+
+        file_path = ""
+
+        if file_paths is not None:
+            file_path = file_paths[0]
+
+        return file_path
+
     def centralize_widget(self):
         """
         This method is responsible for centralising the widget on the screen.
@@ -105,12 +136,45 @@ class AssetizeUI(QtWidgets.QDialog):
         """
         self.ui.pushButton_add_asset.clicked.connect(self.action_add_asset)
         self.ui.pushButton_remove_asset.clicked.connect(self.action_remove_asset)
+        self.ui.pushButton_add_flavour.clicked.connect(self.action_add_flavour)
+        self.ui.pushButton_bring_em_in.clicked.connect(self.action_update_asset)
+
+        self.ui.treeView_assets.selectionModel().selectionChanged.connect(
+            self.selection_changed
+        )
+        self.tree_model.itemChanged.connect(self.asset_modified)
+
+    def selection_changed(self):
+        """
+        Gets triggered every time the selection in the treeView_assets is changed.
+        Since this would event would be triggered more often than others. Try to keep the
+        computations minimal.
+        """
+        self.current_selected_indexes = self.ui.treeView_assets.selectedIndexes()
+
+        if len(self.current_selected_indexes) == 1:
+            self.current_asset_name = (
+                self.current_selected_indexes[0]
+                .model()
+                .itemFromIndex(self.current_selected_indexes[0])
+                .text()
+            )
+
+    def asset_modified(self):
+        """
+        Gets triggered every time an item in the tree model is changed.
+        """
+        self.assetize_backend.update_asset(
+            asset_name=self.current_asset_name,
+            new_asset_name=self.current_selected_indexes[0]
+            .model()
+            .itemFromIndex(self.current_selected_indexes[0])
+            .text(),
+        )
 
     def update_tree_view(self):
         """
         Sets up the initial tree view to which items could be aded later.
-
-        :return:
         """
         self.tree_model.clear()
 
@@ -119,11 +183,17 @@ class AssetizeUI(QtWidgets.QDialog):
 
         for asset in self.assetize_backend.assets:
             # Creating an Asset Tree Item to attach to the tree view
-            asset_item = AssetStandardItem(
-                asset, fg_color=(QtGui.QColor(255, 0, 0, 255))
-            )
+            asset_item = AssetStandardItem(asset)
+
+            for asset_info in self.assetize_backend.assets[asset]["flavours"]:
+                # Creating an Asset Tree Item to attach to the tree view
+                asset_info_item = AssetStandardItem(asset_info)
+
+                asset_item.appendRow(asset_info_item)
 
             self.assets_tree_root_node.appendRow(asset_item)
+
+        self.ui.treeView_assets.expandAll()
 
     def action_add_asset(self):
         """
@@ -131,65 +201,115 @@ class AssetizeUI(QtWidgets.QDialog):
         For now, it mean, that it adds the asset to the assets list in assetize_backend object
         and adds the asset to the UI tree.
         """
-        asset_creation_succesful = 0
+        asset_creation_successful = 0
 
         try:
             # Getting the asset path input from the user
-            asset_ref_path = self.get_asset_path_dialog()
+            flavour_disk_path = self.get_file_path_dialog()
 
-            if asset_ref_path is None:
+            if flavour_disk_path is None:
                 return
 
-            if not asset_ref_path or not os.path.exists(asset_ref_path):
-                raise ValueError("INVALID REFERENCE - {0}".format(asset_ref_path))
+            if not flavour_disk_path or not os.path.exists(flavour_disk_path):
+                raise ValueError("INVALID REFERENCE - {0}".format(flavour_disk_path))
 
             # Generating the asset name from on the file name
-            asset_name = self.assetize_backend.generate_asset_name(asset_ref_path)
+            asset_name = self.assetize_backend.generate_asset_name(flavour_disk_path)
 
             # This will override the asset name based on the actual asset node created in the scene
             asset_name = self.assetize_backend.create_asset_node(asset_name)
 
-            asset_creation_succesful = 1
+            asset_creation_successful = 1
         except Exception as err:
+            # TODO: Try to not keep it this broad of an exception
             raise err
 
-        if asset_creation_succesful:
-            # Registering
-            self.assetize_backend.register_asset(asset_name, asset_ref_path)
+        if asset_creation_successful:
+            # Registering only if the asset creation in the scene was successful
+            self.assetize_backend.register_main_asset(
+                asset_name, flavour_disk_path, fg_color=(255, 0, 0, 255)
+            )
             self.update_tree_view()
+
+    def request_flavour_name(self, msg):
+        cmds.promptDialog(title="Add Flavour", message=msg, button=["Create Flavour"])
+
+        return cmds.promptDialog(q=1, tx=1)
+
+    def action_add_flavour(self):
+        """
+        Performs all necessary actions to register a flavour on the selected asset.
+
+        :return:
+        """
+        if self.current_asset_name not in self.assetize_backend.assets.keys():
+            raise ValueError("INVALID ASSET - {0}".format(self.current_asset_name))
+
+        flavour_name = self.request_flavour_name(
+            msg="ADDING FLAVOUR ON - {1}\n{0}\nGive this Flavour a Name.\t\t\t\t".format(
+                "-" * 60, self.current_asset_name
+            )
+        )
+
+        while True:
+            if (
+                flavour_name
+                in self.assetize_backend.assets[self.current_asset_name][
+                    "flavours"
+                ].keys()
+            ):
+
+                flavour_name = self.request_flavour_name(
+                    msg="ADDING FLAVOUR ON - {1}\n{0}\nFlavour already exists - '{2}'\t\t"
+                    "\t\t\nEnter a New Name".format(
+                        "-" * 60, self.current_asset_name, flavour_name
+                    )
+                )
+            elif flavour_name == "":
+                print "UNABLE TO CREATE FLAVOUR. Flavour Name cannot be empty."
+                return
+            else:
+                break
+
+        self.assetize_backend.add_flavour(
+            asset_name=self.current_asset_name,
+            flavour_name=flavour_name,
+            flavour_path="Empty",
+        )
+
+        self.update_tree_view()
 
     def action_remove_asset(self):
         """
         Removes the Asset from the scene and de-registers from the backend data structure.
         """
-        selected_indexes = self.ui.treeView_assets.selectedIndexes()
+        for selected_index in self.current_selected_indexes:
 
-        for selected_index in selected_indexes:
-            selected_asset_name = selected_index.model().itemFromIndex(selected_index).text()
+            selected_asset_name = (
+                selected_index.model().itemFromIndex(selected_index).text()
+            )
             self.assetize_backend.delete_asset_node(selected_asset_name)
             self.assetize_backend.deregister_asset(selected_asset_name)
             self.tree_model.removeRow(1, selected_index)
 
         self.update_tree_view()
 
-    @staticmethod
-    def get_asset_path_dialog():
+    def action_update_asset(self):
         """
-        Opens up a new FIle Dialog to get the Asset FIle Path from the user.
 
-        :return string: Path to the asset on disk as selected by the user.
+        :return:
         """
-        asset_path = cmds.fileDialog2(
-            startingDirectory=os.path.expanduser("~"),
-            fileFilter="All Files *.*;;Maya Files (*.ma *.mb);;Alembic Files (*.abc)",
-            fileMode=1,
-            okCaption="Add Asset",
-        )
+        selected_indexes = self.ui.treeView_assets.selectedIndexes()
 
-        if asset_path is not None:
-            asset_path = asset_path[0]
+        for selected_index in selected_indexes:
+            selected_asset_name = (
+                selected_index.model().itemFromIndex(selected_index).text()
+            )
+            self.assetize_backend.update_asset(
+                selected_asset_name, fg_color=(0, 255, 0, 255)
+            )
 
-        return asset_path
+        self.update_tree_view()
 
 
 def get_ui_file():
@@ -202,7 +322,7 @@ def get_ui_file():
     return os.path.join(os.path.split(curr_dir)[0], REL_UI_FILE_PATH)
 
 
-def main(width=430, height=550):
+def main(width=950, height=530):
     """
     Main function to be called if this modules is to be imported and used.
 
@@ -227,7 +347,7 @@ def main(width=430, height=550):
     assetize_backend = assetize.Assetize()
 
     my_ui = AssetizeUI(assetize_backend, ui_file, width, height)
-    my_ui.show()
+    my_ui.show(dockable=True)
 
 
 if __name__ == "__main__":
